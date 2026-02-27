4ca28c50b50833d1d27d648af259ec4016929080
Anderson Fariña <inumansoul@gmail.com>
2026-02-27 20:56:43 -0300

    Api updates

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 */
 
-Route::middleware(['auth:sanctum'])->group(function () {
+// Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
@@ -48,4 +48,4 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::middleware('role:super-admin')->group(function () {
         Route::get('/available-owners', [CompanyController::class, 'availableOwners']);
     });
-});
+// });
